1dcd64492e60c6a71d8a1c0d0922e29a16e7733b
Ryan Thissen <rmt1855@gmail.com>
2017-04-23 16:52:34 -0700

    added form back button component, revenue & cost form component

---
+++ src/components/forms/revenue-costs/revenue-costs-submit-button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RevenueCostsSubmitButton = () => (
+  <div id="revenue-costs-submit-button" className="column eight wide">
+    <button className="ui button">Continue To Sources &gt;</button>
+  </div>
+);
+
+export default RevenueCostsSubmitButton;
