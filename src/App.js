4930f548906d59018d4d376eb1e94b74426393b7
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-04-22 17:28:16 -0700

    added react router logic

--- src/App.js
+++ src/App.js
@@ -7,18 +7,32 @@ import About from './components/home/about';
 import SupportImage from './components/home/support-image';
 import Details from './components/home/details';
 import Footer from './components/footer/footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavbarHome />
-        <MainImage />
-        <About />
-        <SupportImage />
-        <Details />
-        <Footer />
+    <Router>
+      <div className ="App">
+        <Route exact path="/" component={NavbarHome} />
+        <Route exact path="/" component={MainImage} />
+        <Route exact path="/" component={About} />
+        <Route exact path="/" component={SupportImage} />
+        <Route exact path="/" component={Details} />
+        <Route path="/" component={Footer} />
       </div>
+    </Router>
+
+
+
+      // <div className="App">
+      //   <NavbarHome />
+      //   <MainImage />
+      //   <About />
+      //   <SupportImage />
+      //   <Details />
+      //   <Footer />
+      // </div>
     );
   }
 }
