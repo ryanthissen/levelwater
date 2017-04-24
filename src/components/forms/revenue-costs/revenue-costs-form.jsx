d5def710c86d6fdd2806e5e00e7c82e5ce427112
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-04-24 10:46:40 -0700

    refactored revenue cost form using redux form

--- src/components/forms/revenue-costs/revenue-costs-form.jsx
+++ src/components/forms/revenue-costs/revenue-costs-form.jsx
@@ -1,52 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SignUpStep from '../sign-up-step';
 import RevenueCostsSubmitButton from './revenue-costs-submit-button';
-import SignUpBackButton from '../sign-up-back-button'
+import SignUpBackButton from '../sign-up-back-button';
+import { Field, reduxForm } from 'redux-form';
 
-const RevenueCostsForm = () => (
-  <div id="revenue-costs-form">
-    <SignUpStep step="2" />
-    <div className="ui grid">
-      <div className="column sixteen wide">
-        <h3>Please Enter Revenue, Cost, and Reserve Fund Information From Your Most Recent Year of Operations</h3>
-      </div>
-    </div>
-
-    <form id="step2">
-      <div className="ui grid">
-        <div className="column seven wide">
-          <label >Average Monthly Rate per Connection:</label>
-          <label >Annual Revenue From Water Sales:</label>
-          <label >Annual Revenue From Fees And Charges:</label>
-          <label >Annual Revenue From Subsidies:</label>
-          <label >Total Amount in Reserve Fund:</label>
-          <label >Annual Reserve Fund Contribution:</label>
-          <label >Annual Personnel Costs:</label>
-          <label >Annual Operations Costs:</label>
-          <label >Annual Debt Financing:</label>
+class RevenueCostsForm extends Component {
+  render() {
+    return (
+      <div id="revenue-costs-form">
+        <SignUpStep step="2" />
+        <div className="ui grid">
+          <div className="column sixteen wide">
+            <h3>Please Enter Revenue, Cost, and Reserve Fund Information From Your Most Recent Year of Operations</h3>
+          </div>
         </div>
 
-        <div className="column nine wide">
-          <input />
-          <input />
-          <input />
-          <input />
-          <input />
-          <input />
-          <input />
-          <input />
-          <input />
-        </div>
+        <form id="step2">
+          <div className="ui grid">
+            <div className="column seven wide">
+              <label >Average Monthly Rate per Connection:</label>
+              <label >Annual Revenue From Water Sales:</label>
+              <label >Annual Revenue From Fees And Charges:</label>
+              <label >Annual Revenue From Subsidies:</label>
+              <label >Total Amount in Reserve Fund:</label>
+              <label >Annual Reserve Fund Contribution:</label>
+              <label >Annual Personnel Costs:</label>
+              <label >Annual Operations Costs:</label>
+              <label >Annual Debt Financing:</label>
+            </div>
+
+            <div className="column nine wide">
+              <Field name="ratePerConnection" component="input" type="number" />
+              <Field name="waterSalesRev" component="input" type="number" />
+              <Field name="feesChargesRev" component="input" type="number" />
+              <Field name="subsidiesRev" component="input" type="number" />
+              <Field name="reserveFundTotal" component="input" type="number" />
+              <Field name="reserveFundContribution" component="input" type="number" />
+              <Field name="personnaelCosts" component="input" type="number" />
+              <Field name="operationCosts" component="input" type="number" />
+              <Field name="debtFinancing" component="input" type="number" />
+            </div>
+          </div>
+          <div className="ui grid">
+            <SignUpBackButton />
+            <div id="revenue-costs-submit-button" className="column eight wide">
+              <button className="ui button">Continue To Sources &gt;</button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div className="ui grid">
-        <SignUpBackButton />
-        <div id="revenue-costs-submit-button" className="column eight wide">
-          <button className="ui button">Continue To Sources &gt;</button>
-        </div>
-      </div>
-    </form>
-  </div>
-)
+    );
+  }
+}
+
+RevenueCostsForm = reduxForm({
+  form: 'revenueCosts'
+})(RevenueCostsForm);
+
 
 
 export default RevenueCostsForm;
