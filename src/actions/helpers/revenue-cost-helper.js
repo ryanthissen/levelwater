623b01f0e2a217285d7f9e6b20322f6eee35dff0
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-05-01 14:13:35 -0700

    changed revenue cost helper var names to match back end

--- src/actions/helpers/revenue-cost-helper.js
+++ src/actions/helpers/revenue-cost-helper.js
@@ -17,11 +17,11 @@ const revenueCostHelper = ( current_average_water_rate, total_financial_reserves
       current_average_water_rate: parseInt(current_average_water_rate),
       total_financial_reserves: parseInt(total_financial_reserves),
       annual_revenue_water_sales: parseInt(annual_revenue_water_sales),
-      annual_revenue_fees_charges: parseInt(annual_revenue_fees_charges),
+      annual_revenue_fees_charged: parseInt(annual_revenue_fees_charges),
       annual_revenue_subsidies: parseInt(annual_revenue_subsidies),
-      annual_savings_to_financial_reserve: parseInt(annual_savings_to_financial_reserve),
+      annual_savings_to_financial_reserves: parseInt(annual_savings_to_financial_reserve),
       annual_personnel_costs: parseInt(annual_personnel_costs),
-      annual_operational_costs: parseInt(annual_operational_costs),
+      annual_operations_costs: parseInt(annual_operational_costs),
       annual_debt_costs: parseInt(annual_debt_costs)
     }, config)
     .then((response) => {
