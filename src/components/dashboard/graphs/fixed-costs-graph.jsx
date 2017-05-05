32a9bb3bfb96ec54f7fbd4df34f46dc446f5a1d2
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-05-04 18:11:01 -0700

    got fixed cost graph to take live data

--- src/components/dashboard/graphs/fixed-costs-graph.jsx
+++ src/components/dashboard/graphs/fixed-costs-graph.jsx
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import C3Chart from 'react-c3js';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const data = {
-  columns: [
-      ['Personnel Costs', 50000],
-      ['Operational Costs', 13950],
-      ['Debt Costs', 20000],
-  ],
-  type : 'donut'
+const mapStateToProps = (state, ownProps) => {
+  return {
+    dashState: state.dashButtons.ratesFinancesObject
+  };
 };
+
+
 
 const donut = {
   title: 'Fixed Costs',
-}
+};
 
 
-export default class FixedCostsGraph extends Component {
+class FixedCostsGraph extends Component {
+
+  constructor(props) {
+    super(props);
+    this.data = {
+      columns: [
+          ['Personnel Costs', this.props.dashState.annual_personnel_costs],
+          ['Operational Costs', this.props.dashState.annual_personnel_costs],
+          ['Debt Costs', this.props.dashState.annual_debt_costs],
+      ],
+      type : 'donut'
+    };
+  }
 
   render() {
     return (
     <div className="c3" id="c3-visuals">
-      <C3Chart data={data} donut={donut} />
-
+      <C3Chart data={this.data} donut={donut} />
     </div>
     );
-
   }
 }
+
+export default connect(mapStateToProps)(FixedCostsGraph);
