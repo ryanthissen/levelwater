74dd24cdb9797dc37ecc4f30e4f182f70ef14e2e
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-05-05 16:40:42 -0700

    added demo button funcitonality

--- src/components/nav/demo-button.jsx
+++ src/components/nav/demo-button.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { logUserIn } from '../../actions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router,
+Route,
+Link,
+} from 'react-router-dom';
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators ({ logUserIn }, dispatch);
+};
+
+export class DemoButton extends Component{
+  render() {
+    return (
+      <Route render={({ history }) => (
+        <button className="ui button" onClick={(event) => {
+          event.preventDefault();
+          this.props.logUserIn('alex83@gmail.com', 'something',() => {
+            history.push('/dashboard');
+          });
+        }}>Try It!</button>
+        )} />
+    );
+  }
+}
 
 
 
-const DemoButton = () => (
-  <button className="ui button">Try It!</button>
-);
 
-export default DemoButton;
+
+export default connect (mapDispatchToProps)(DemoButton);
