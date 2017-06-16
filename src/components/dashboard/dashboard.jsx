3afb33a92a980ea47c1ab79b3534027988775c64
Ryan Thissen <rmt1855@gmail.com>
2017-06-16 11:59:31 -0700

    changed styling

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import DashHeader from './dash-header';
 import C3Visuals from './c3-visuals';
+import DashAnalysis from './dash-analysis';
 import DashButtons from './dash-buttons';
 import styles from './dashboard.css';
+
 
 const Dashboard = () => (
   <div id="dashboard">
     <DashHeader />
     <C3Visuals />
+    {/* <DashAnalysis /> */}
     {/* <DashButtons /> */}
   </div>
 );
 
 export default Dashboard;
+
+
+// financialData={this.props.ratesFinancesObject} criticalInfrastructure={this.props.criticalInfrastructure} noncriticalInfrastructure={this.props.noncriticalInfrastructure}
--- src/components/dashboard/graphs/rate-graph.jsx
+++ src/components/dashboard/graphs/rate-graph.jsx
@@ -11,8 +11,6 @@ const mapStateToProps = (state, ownProps) =>  {
     ratesFinancesObject : state.dashButtons.ratesFinancesObject,
     criticalInfrastructure : state.dashButtons.criticalInfrastructure,
     noncriticalInfrastructure : state.dashButtons.noncriticalInfrastructure
-
-
   };
 };
 
@@ -36,25 +34,19 @@ const axis = {
 
     };
 
-
-
 const zoom = {
       enabled: true
     };
 
-
-
 const size =  {
         height: 320,
-        width: 680
+        width: 660
     };
-
 
 class RateGraph extends Component {
 
   componentDidMount() {
     this.props.loadChart();
-
   }
 
   getDataAndLines() {
@@ -63,7 +55,7 @@ class RateGraph extends Component {
     let noncriticalInfrastructure = this.props.noncriticalInfrastructure;
 
 
-    console.log('info', criticalInfrastructure)
+    // console.log('info', criticalInfrastructure)
 
     let increase = ratesFinancesObject.annual_savings_to_financial_reserves;
     let years = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ];
@@ -92,11 +84,8 @@ class RateGraph extends Component {
                   ['x', '2017-07-24', '2018-07-25', '2019-07-26', '2020-07-27', '2021-07-28', '2022-07-29', '2023-07-29', '2024-07-29', '2025-07-29', '2026-07-29', '2027-07-24', '2028-07-25', '2029-07-26', '2030-07-27', '2031-07-28', '2032-07-29', '2033-07-29', '2034-07-29', '2035-07-29', '2036-07-29', '2037-07-24', '2038-07-25', '2039-07-26', '2040-30-27', '2041-07-28', '2042-07-29', '2043-07-29', '2044-07-29', '2045-07-29', '2046-07-29', '2047-07-29', '2048-07-29', '2049-07-29', '2050-07-29', '2051-07-29', '2052-07-29', '2053-07-29', '2054-07-29', '2055-07-29', '2056-07-29'],
                   ['annual contribution to reserve', increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase, increase],
                   ['current rate', years[0], years[1], years[2], years[3], years[4], years[5], years[6], years[7], years[8], years[9],years[10], years[11], years[12], years[13], years[14],years[15], years[16], years[17], years[18], years[19],years[20], years[21], years[22], years[23], years[24],years[25], years[26], years[27], years[28], years[29],years[30], years[31], years[32], years[33], years[34],years[35], years[36], years[37], years[38], years[39]]
-
               ],
-
               type: 'line',
-
           }
     }
 
@@ -117,15 +106,10 @@ class RateGraph extends Component {
       <div className="ui grid">
         <FixedCostsGraph />
         <div id="charts" className="column eight wide">
-            <C3Chart data={data} axis={axis} grid={grid} size={size} />
+          <C3Chart data={data} axis={axis} grid={grid} size={size} />
+        </div>
+        <DashAnalysis financialData={this.props.ratesFinancesObject} criticalInfrastructure={this.props.criticalInfrastructure} noncriticalInfrastructure={this.props.noncriticalInfrastructure} />
       </div>
-
-
-      <DashAnalysis financialData={this.props.ratesFinancesObject} criticalInfrastructure={this.props.criticalInfrastructure} noncriticalInfrastructure={this.props.noncriticalInfrastructure} />
-
-
-      </div>
-
     </div>
     );
 
