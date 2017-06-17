3e2b774d8fb5a74f5575316881f639b6207f4f31
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-06-17 12:43:12 -0700

    form mobile styles

--- src/components/forms/sign-up-step.jsx
+++ src/components/forms/sign-up-step.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const SignUpStep = (props) => (
   <div id="sign-up-step" className="ui grid">
-    <div className="column thirteen wide"></div>
-    <div className="column three wide">
+    <div className="column thirteen wide computer one wide tablet"></div>
+    <div className="column three wide computer five wide tablet">
       Step {props.step} of 6
     </div>
   </div>
