71bed62acab26ac1290b7667144f231ce2b222c8
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-06-18 14:55:39 -0700

    form responsiveness

--- src/components/forms/revenue-costs/revenue-costs-submit-button.jsx
+++ src/components/forms/revenue-costs/revenue-costs-submit-button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const RevenueCostsSubmitButton = () => (
-  <div id="revenue-costs-submit-button" className="column eight wide">
+  <div id="revenue-costs-submit-button" className="column eight wide computer sixteen wide tablet">
     <button className="ui button">Sources &gt;</button>
   </div>
 );
--- src/components/forms/sign-up-back-button.jsx
+++ src/components/forms/sign-up-back-button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const SignUpBackButton = (props) => (
-  <div id="sign-up-back-button" className="column eight wide">
+  <div id="sign-up-back-button" className="column eight wide computer sixteen wide tablet">
         <Link to={props.to}><button className="ui button">&lt; Go Back</button></Link>
   </div>
 );
